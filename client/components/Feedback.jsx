34109ff39e76f617bbe1a6e3a0760c89850d12b4
Karel Fourie <karelmakazi@gmail.com>
2020-10-26 10:10:19 +1300

    Add initial functionality Feedback

---
+++ client/components/Feedback.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Feedback = props => {
+  const feedbackStatus = 'spread'
+
+  return (
+    <div className=''>
+      <h2>{feedbackStatus}</h2>
+    </div>
+  )
+}
+
+export default Feedback
